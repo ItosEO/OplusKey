1d85e2fc3fcf3cb306cb51e2fcf8d5d795bb6d30
ItosEO <m2265514616@163.com>
2025-08-31 20:13:52 +0800

    refactor: 重命名模块并更新脚本
    将模块ID从 `tri_exp_caelifall` 更改为 `action_exp`。
    将模块名称从 `一加の三段键拓展` 更改为 `Oplus侧键拓展`。
    更新了版本号和作者信息。
    将模块描述更新为 `拓展你的一加侧键功能(三段式和快捷键)`。
    
    在 `cust.sh` 脚本中:
    - 添加了 `cust_click_script` 函数用于处理单击事件。
    - 添加了 `cust_long_script` 函数用于处理长按事件。
    - 在现有的单击和长按逻辑中调用了这些新函数。

--- app/src/magisk_template/cust.sh
+++ app/src/magisk_template/cust.sh
@@ -1,9 +1,21 @@
 #!/system/bin/sh
 IS_LONG_CLICK=${1:-false}
+
+function cust_click_script() {
+    # 在这里编写单击时执行的脚本
+}
+
+function cust_long_script() {
+    # 在这里编写长按时执行的脚本
+}
+
 if $IS_LONG_CLICK; then
     # long click
     echo "long click"
+    cust_long_script
 else
     # single click or double click
     echo "single click or double click"
+    cust_click_script
 fi
+
